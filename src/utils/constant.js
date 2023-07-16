6001e9dadaac0de248e5f8bb8ff9af13774e63ed
Harsh723 <rajharsh4424@gmail.com>
2023-07-17 00:13:44 +0530

    restructing folder & import/export

---
+++ src/utils/constant.js
@@ -0,0 +1,6 @@
+//it is recommended to use Snake case for storing url
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/";
+
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRDwnOKl41cCXsIRaX8_KTXTWjYXdC6dUmXQ&usqp=CAU";
