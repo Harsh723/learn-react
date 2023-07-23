87e12d375245fd93acf7bb30e3f1c31ab612d290
Harsh723 <rajharsh4424@gmail.com>
2023-07-24 00:47:25 +0530

    shimmer ui & api call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 console.log(React); // object
 
@@ -14,6 +18,21 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,78 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+//import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  return (
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    //Optional Chaining
+    setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
+  //conditional rendering
+  //if (listOfRestaurant.length === 0) return <Shimmer />;
+  console.log("body rendered");
+
+  //another way to write conditional rendering is to use ternary operator
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+              const filterRestaurant = listOfRestaurant.filter(
+                (res) =>
+                  //res.data.name.includes(searchText) - this will not work for all the cases and people generally stuck here
+                  res.data.name.toLowerCase().includes(searchText.toLowerCase()) // better way
+                //incase of empty text how inclues returning true for all the cases ?
+              );
+              setFilteredRestaurant(filterRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurant = listOfRestaurant.filter(
+              (val) => val.data.avgRating > 4
+            );
+            setFilteredRestaurant(filteredRestaurant);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((res) => (
+        {filteredRestaurant.map((res) => (
           <RestaurantCard key={res.data.id} resData={res} />
         ))}
       </div>
